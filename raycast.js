75b5bf82a85d29840f7adba13830674347daacb8
asceznyk <asceznyk@gmail.com>
2021-06-29 21:36:33 +0530

    crappy minimap

--- raycast.js
+++ raycast.js
@@ -1,3 +1,13 @@
+//first some utility functions
+
+function toRadians(deg) {
+  return (deg * Math.PI) / 180;
+}
+
+function distance(x1, y1, x2, y2) {
+  return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+}
+
 const map = [
   [1, 1, 1, 1, 1, 1, 1, 1],
   [1, 0, 0, 0, 0, 0, 0, 1],
@@ -15,17 +25,7 @@ const tick = 30;
 const scwidth = window.innerWidth;
 const scheight = window.innerHeight;
 
-const csize = 24;
-
-//utility funcions
-
-function toRadians(deg) {
-  return (deg * Math.PI) / 180;
-}
-
-function distance(x1, y1, x2, y2) {
-  return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-}
+const cellsize = 32;
 
 const canvas = document.createElement("canvas");
 canvas.setAttribute("width", scwidth);
@@ -34,13 +34,29 @@ document.getElementById("wrap").appendChild(canvas);
 
 const ctx = canvas.getContext('2d');
 
-function renderMinimap(posx, posy, scale=0) {
+class Player {
+  constructor() {
+    this.x = cellsize * 1.5;
+    this.y = cellsize * 2;
+    this.angle = 0;
+    this.speed = 0;
+  }
+}
+
+const player = new Player();
+
+function renderMinimap(posx, posy, scale=0.75) {
+  let csize = cellsize * scale;
+
 	map.forEach((row, y) => {
 		row.forEach((cell, x) => {
 			ctx.fillStyle = cell ? '#999' : '#000'; 
 			ctx.fillRect(posx+(x*csize), posy+(y*csize), csize, csize);
 		})
-	})
+	});
+
+  ctx.fillStyle = 'blue';
+  ctx.fillRect(posx + player.x * scale - 10/2, posy + player.y * scale - 10/2, 10, 10);
 }
 
 function gameLoop() {
